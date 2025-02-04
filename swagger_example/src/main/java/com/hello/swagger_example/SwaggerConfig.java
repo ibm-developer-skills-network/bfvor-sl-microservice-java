1c0d71e7116bae936008f3fb091488f618a8c616
harsh <harsh.singh@skillup.tech>
2025-02-04 22:04:32 +0530

    swagger example app

---
+++ swagger_example/src/main/java/com/hello/swagger_example/SwaggerConfig.java
@@ -0,0 +1,11 @@
+package com.hello.swagger_example;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+   
+
+
+}
---
+++ swagger_example/src/test/java/com/hello/swagger_example/SwaggerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.swagger_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
